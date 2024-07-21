e9c360b61b9f9db9ecc1340d0050cbe9c932e602
chrisarg <chrisarg@gmail.com>
2024-07-21 01:11:55 -0600

    print_rax from 2.3/load data->rax/print newline

---
+++ Chapter_2_Assembly_Language/print_rax.asm
@@ -0,0 +1,54 @@
+; Path: print_rax.asm
+; Assemble/link/run with:
+; nasm -f elf64 print_rax.asm
+; ld -o print_rax.exe print_rax.o
+; ./print_rax.exe ; echo " Exit with status:  $? "
+
+global _start
+section .data
+codes: 
+    db '0123456789ABCDEF'
+
+value:
+    dq 0x123456789ABCDEF0
+
+newline:
+    db 10
+
+print_len: equ 1    ; print a single character
+field_len: equ 64   ; field length is 64 bits
+
+section .text
+_start:
+    ; Print a single message
+    mov rax, [value]    ; set rax to the 8 byte value we would like to print
+    mov rdi, 1          ; file descriptor 1 is stdout
+    mov rdx, print_len  ; print single character
+    mov rcx, field_len  ; field length is 64 bits
+
+.loop:
+    push rax
+    sub rcx, 4
+    sar rax, cl
+    and rax, 0xF
+
+    lea rsi, [codes+rax]
+    ; prepare for syscall, need to preserve rax and rcx
+    mov rax,1          ; syscall number for sys_write
+    push rcx
+    syscall
+
+    mov rsi,newline       ; print a newline
+    syscall
+    ; restore rax and rcx
+    pop rcx
+    pop rax
+
+    test rcx, rcx
+    jnz .loop
+
+    ; Exit
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
